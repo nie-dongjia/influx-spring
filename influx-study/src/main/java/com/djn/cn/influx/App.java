022ed49cdd69f8c9696db076c3e0b0e039ffd4e0
NieDongjia <327803131@qq.com>
2020-06-13 21:33:36 +0800

    Initial commit

---
+++ influx-study/src/main/java/com/djn/cn/influx/App.java
@@ -0,0 +1,13 @@
+package com.djn.cn.influx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
